afe74cf0721edf905b3bc0f3f0e41e78de2c5f85
captianrandom <psudo.random@gmail.com>
2016-06-14 06:19:39 -0700

    Now writing out results.  Need to figure out how to populate the # of tweets.

--- AnalizeResults.py
+++ AnalizeResults.py
@@ -1,5 +1,6 @@
 import simplejson
 from numpy import genfromtxt
+import csv
 
 class CompanyName():
 
@@ -19,13 +20,21 @@ class CompanyName():
     def __str__(self):
         return str({ "company_name": self.companyName, "executiveName": self.executiveName })
 
+
+def generateProfilePageUrl(user):
+    return 'https://twitter.com/' + user['screen_name']
+
 class CompanyTwitterAccount():
     def __init__(self, company, user):
         self.company = company
         self.profileUrl = generateProfilePageUrl(user)
-        self.numTweets = user['retweet_count'] # not really sure that this exists
+        self.numTweets = user.get('retweet_count', None) # not really sure that this exists
         self.numFollowers = user['followers_count']
         self.numFavorites = user['favourites_count']
+
+    def asList(self):
+        return [self.company.getCompanyName(), self.company.getExecutiveName(), self.profileUrl, self.numTweets, self.numFollowers,
+                self.numFavorites]
 
 json_file = open("chase_search_results.json", 'r')
 jsonResponse = simplejson.load(json_file)
@@ -35,33 +44,50 @@ for user in jsonResponse:
     print data
     print
 
+def matchUserToCompany(company, user):
+    listOfAttributesToCheck = ['screen_name', 'name']
+    for attribute in listOfAttributesToCheck:
+        if matchNameToCompany(company, user[attribute]):
+            return True
+    return False
+
 def matchNameToCompany(company, name):
-    if name.lower() == company.executiveName.lower():
+    if name.lower() == company.getExecutiveName().lower():
         return True
-    elif name.lower() == company.companyName.lower():
+    elif name.lower() == company.getCompanyName().lower():
         return True
     else: # need some slightly looser definition of matching
         return False
 
+def findAndBuildTwitterCompnayAccount(companyName, userList):
+    for user in userList:
+            if matchUserToCompany(companyName, user):
+                return CompanyTwitterAccount(companyName, user)
+
 csvFileName = 'test.csv'
 print "Reading in CSV from " + csvFileName
 with open(csvFileName, 'r') as csvfile:
+    # build header info
     data = genfromtxt(csvfile, delimiter=',', dtype=None)
     headerMap = dict(zip(data[0], xrange(0,len(data[0]))))
+
     foundCompanies = []
     for row in data[1:]:
+        # read in data about each company
         companyName = CompanyName(row, headerMap)
         print companyName
-        # TODO: Make request to twitter here
+
         requestList = None
-        for user in requestList:
-            if matchNameToCompany(companyName, user):
-                foundCompanies.append(CompanyTwitterAccount(companyName))
+        companyTwitterAccount = findAndBuildTwitterCompnayAccount(companyName, jsonResponse)
+        if companyTwitterAccount:
+            foundCompanies.append(companyTwitterAccount)
 
-# TODO: write out twitter data
-destinationCsvFileName = None
-for twitterAccount in foundCompanies:
-    pass
 
-def generateProfilePageUrl(user):
-    return 'https://twitter.com/' + user['screen_name']
+print
+print "num companies found " + str(len(foundCompanies))
+destinationCsvFileName = 'output.csv'
+print "Writing results to " + destinationCsvFileName
+with open(destinationCsvFileName, 'w') as fp:
+    writer = csv.writer(fp, delimiter=',')
+    data = [twitterAccount.asList() for twitterAccount in foundCompanies]
+    writer.writerows(data)
